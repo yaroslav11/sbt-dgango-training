2b13238b33b1baf37d57def0f9fb156a1af38d78
yaroslav11 <yaroslav.zhukov@phystech.edu>
2017-10-12 15:25:02 +0300

    Evening 12 october 2017 Added: Get/Post requests, JS, Ajax

--- application/urls.py
+++ application/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'app_urls'
+
 urlpatterns = [
     # ex: /application/
     url(r'^$', views.index, name='index'),
     # ex: /application/5/
+    # the 'name' value as called by the {% url %} template tag
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /application/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /application/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    url(r'^mng/$', views.mng, name='mng'),
+    url(r'^delete_question/$', views.delete_question, name='delete_question'),
+    # url(r'^delete_question/[\S]+$', views.delete_question, name='delete_question'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,30 +1,20 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.db import transaction, connection
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.urls import reverse
+import json
 
 from application.models import *
 
-
-# def index(request):
-    # return HttpResponse("Hello, world. You're at the appplication/views.py index.")
-
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = "\n".join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-
-
-
-
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:20]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'application/index.html', context)
 
 
-
 def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
@@ -32,8 +22,51 @@ def detail(request, question_id):
     return render(request, 'application/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'application/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'application/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('app_urls:results', args=(question.id,)))
+
+def mng(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:20]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'application/mng.html', context)
+
+@transaction.atomic
+def delete_question(request):
+    if request.method == 'GET':
+        qid = request.GET.get('question_id')
+        question = get_object_or_404(Question, pk=qid)
+        question.delete()
+
+        for q in connection.queries:
+            print(q)
+
+        return HttpResponse(json.dumps({"message": "All ok with GET"}), content_type='application/json')
+
+    elif request.method == "POST":
+        qid = request.POST.get('question_id')
+        question = get_object_or_404(Question, pk=qid)
+        question.delete()
+
+        return HttpResponse(json.dumps({"message": "All ok with POST"}), content_type='application/json')
+
+    else:
+        return HttpResponse(json.dumps({"message": "Unknown request method"}), content_type='application/json')
